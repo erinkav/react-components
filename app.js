ca1b350c5f6ae31d5f531116a011577c07943722
Erin Kavanaugh <elkavanaugh@gmail.com>
2016-05-06 11:56:06 -0700

    Refactored Grocery List

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-// TODO
+// var GroceryList = React.createClass ({
+	
+// 	render: function () {
+// 		return (
+// 			<div>
+// 				<ul>
+// 					<li>Peas</li>
+// 					<li>Carrots</li>
+// 				</ul>
+// 			</div>
+// 		)
+// 	}
+// });
+
+// ReactDOM.render(<GroceryList />), document.getElementById('body')); 
+class GroceryListItem extends React.Component {
+	constructor (props) {
+		super(props); 
+		this.state = {
+			bought: false
+		}
+	}
+	onListItemClick() {
+		this.setState({
+			done: !this.state.done
+		})
+	}
+	render() {
+		var style = {
+			textDecoration: this.state.bought ? 'heavy' : 'none'
+		}
+		return (
+			<li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.groceryItem}</li>
+		);	
+	}
+}
+
+var GroceryList = (props) => (
+	<div>
+		<ul>
+			<h2>my Grocery List </h2>
+			{props.groceryItem.map(groceryItem =>
+
+				<GroceryListItem groceryItem={groceryItem}/>
+			)}
+		</ul>
+	</div>
+);
+
+ReactDOM.render(<GroceryList groceryItem={['kale', 'cucumber', 'bread']}/>, document.getElementById('app')); 
